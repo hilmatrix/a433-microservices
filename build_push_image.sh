aa518d5b845be6aa48b886fc21141658b932ec71
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2022-11-17 19:03:43 +0700

    menyempurnakan script build dan membuat docker-compose.yml

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,1 +1,5 @@
 docker build -t item-app:v1 .
+docker image ls
+docker tag item-app:v1 hilmatrix/item-app:v1
+echo $DOCKER_PASSWORD | docker login -u hilmatrix --password-stdin
+docker push hilmatrix/item-app:v1
