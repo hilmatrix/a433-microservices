94b1ced926d376a76e427399d49110978f897f87
DESKTOP-JDH895S\Hilmatrix <hilmaul@gmail.com>
2022-11-22 02:40:40 +0700

    push image yg dibuat ke github packages

--- build_push_image.sh
+++ build_push_image.sh
@@ -4,11 +4,17 @@ docker build -t item-app:v1 .
 # menampilkan daftar image
 docker image ls
 
-# menargetkan image yg dibuat untuk upload ke docker hub 
-docker tag item-app:v1 hilmatrix/item-app:v1
-
 # login docker
 echo $DOCKER_PASSWORD | docker login -u hilmatrix --password-stdin
 
-# upload image yg dibuat
+# login github 
+echo $GITHUB_TOKEN | docker login ghcr.io -u hilmatrix --password-stdin
+
+# menargetkan image yg dibuat untuk di-upload ke docker hub
+docker tag item-app:v1 hilmatrix/item-app:v1
 docker push hilmatrix/item-app:v1
+
+# menargetkan image yg dibuat untuk upload ke ghcr.io
+# image ini nanti yang dipakai lewat docker-compose.yml
+docker tag item-app:v1 ghcr.io/hilmatrix/item-app:v1
+docker push ghcr.io/hilmatrix/item-app:v1
